19b3fbe2a8086e8ec280727861c80dd660987f44
Debys <43010092+HypnosProxy@users.noreply.github.com>
2018-09-10 15:48:10 -0300

    Update ListaConvidado.py

--- ListaConvidado.py
+++ ListaConvidado.py
@@ -1,15 +1,10 @@
 '''
 Exercicio: Faça um programa que leia a quantidade de pessoas que serão convidadas,
-Após isso  o mesmo ira perguntar quais é o nome de cada um.
-Colocar os itens em uma lista apos isso ira imprimir todos os nomes da lista.
+Após isso  o mesmo ira perguntar quais é o nome de cada pessoa, seguidamente os itens sera adicionado em um lista, no final
+a lista sera mostrada na tela.
 '''
 
-#lista =[]
-#q = int(input('Digite a quantidade de convidados: '))
-#for i in range(0,q):
-#    n = str(input('Nome do  convidado: '))
-#    lista += n
-#print(lista)
+
     
 lista = []
 i = 0    
